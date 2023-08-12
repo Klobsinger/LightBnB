3119cdbf83afbab64d5d765a8254b9b67f200c4d
Kevin Lobsinger <kevlobsinger@gmail.com>
2023-08-12 16:32:35 -0400

    Feature: schema Add two tables guest_reviews & rates to database guest_reviews table for owners to rate guests that stay in their bnb rates table for owners to change the cost per night for a certain period

--- migrations/01_schema.sql
+++ migrations/01_schema.sql
@@ -1,8 +1,13 @@
+-- Drop existing tables if they exist
+-- This ensures a clean slate for table creation
 DROP TABLE IF EXISTS users CASCADE;
 DROP TABLE IF EXISTS properties CASCADE;
 DROP TABLE IF EXISTS reservations CASCADE;
 DROP TABLE IF EXISTS property_reviews CASCADE;
+DROP TABLE IF EXISTS rates CASCADE;
+DROP TABLE IF EXISTS guest_reviews CASCADE;
 
+-- Create 'users' table to store user information
 CREATE TABLE users (
   id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(255) NOT NULL,
@@ -10,6 +15,7 @@ CREATE TABLE users (
   password VARCHAR(255) NOT NULL
 );
 
+-- Create 'properties' table to store property listings
 CREATE TABLE properties (
   id SERIAL PRIMARY KEY NOT NULL,
   owner_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
@@ -32,6 +38,7 @@ CREATE TABLE properties (
   active BOOLEAN NOT NULL DEFAULT TRUE
 );
 
+-- Create 'reservations' table to store reservation information
 CREATE TABLE reservations (
   id SERIAL PRIMARY KEY NOT NULL,
   start_date DATE NOT NULL,
@@ -40,6 +47,7 @@ CREATE TABLE reservations (
   guest_id INTEGER REFERENCES users(id) ON DELETE CASCADE
 );
 
+-- Create 'property_reviews' table to store reviews by guests for properties
 CREATE TABLE property_reviews (
   id SERIAL PRIMARY KEY NOT NULL,
   guest_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
@@ -47,4 +55,23 @@ CREATE TABLE property_reviews (
   reservation_id INTEGER REFERENCES reservations(id) ON DELETE CASCADE,
   rating SMALLINT NOT NULL DEFAULT 0,
   message TEXT
+);
+
+-- Create 'guest_reviews' table to store reviews by hosts for guests
+CREATE TABLE guest_reviews (
+id SERIAL PRIMARY KEY NOT NULL,
+guest_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+owner_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+reservation_id INTEGER REFERENCES reservations(id) ON DELETE CASCADE,
+rating SMALLINT NOT NULL DEFAULT 0,
+message TEXT
+);
+
+-- Create 'rates' table to store pricing information for properties
+CREATE TABLE rates (
+  id SERIAL PRIMARY KEY NOT NULL,
+  start_date DATE NOT NULL,
+  end_date DATE NOT NULL,
+  cost_per_night INTEGER  NOT NULL DEFAULT,
+  property_id INTEGER REFERENCES properties(id) ON DELETE CASCADE
 );
